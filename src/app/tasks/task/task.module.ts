fb08a3ec4cf2c3dcb77b83297d07e7c77dfe490d
Nikita Kuznetsov <kuzn5298@gmail.com>
2025-01-09 00:57:16 +0100

    👨‍🔧 Rework shared module

--- src/app/tasks/task/task.module.ts
+++ src/app/tasks/task/task.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { DatePipe } from '@angular/common';
 import { TaskComponent } from './task.component';
-import { CardModule } from '../../shared/card/card.module';
+import { SharedModule } from '../../shared/shared.module';
 
 @NgModule({
   declarations: [TaskComponent],
-  imports: [CardModule, DatePipe],
+  imports: [SharedModule, DatePipe],
   exports: [TaskComponent],
 })
 export class TaskModule {}
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
-import { CardModule } from '../shared/card/card.module';
 import { UserComponent } from './user.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [UserComponent],
-  imports: [CardModule],
+  imports: [SharedModule],
   exports: [UserComponent],
 })
 export class UserModule {}
