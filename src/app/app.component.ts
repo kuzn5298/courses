6d725c40471105eed73befb495eeecbf62bfaab5
Nikita Kuznetsov <kuzn5298@gmail.com>
2025-01-09 13:47:01 +0100

    🔝 Add header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [HeaderComponent],
 })
-export class AppComponent {
-  title = 'investment-calculator';
-}
+export class AppComponent {}
