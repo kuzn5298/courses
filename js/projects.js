e68f6c8730d62bf28c6a19ed44e813850fe54ab7
Nikita Kuznetsov <kuzn5298@gmail.com>
2025-01-10 14:49:30 +0100

    👨‍🔧 Refactoring design

---
+++ js/projects.js
@@ -0,0 +1,35 @@
+const COURSES_URL =
+  'https://raw.githubusercontent.com/kuzn5298/courses/main/courses.json';
+
+const getCourses = async () => {
+  const response = await fetch(COURSES_URL);
+  const courses = await response.json();
+
+  return courses;
+};
+
+const parseCoursesToProjects = (courses) => {
+  return courses
+    .map((course) =>
+      course.projects?.map((project) => ({
+        name: project.name,
+        url: project.url,
+        description: project.description,
+        preview: project.preview,
+        github: project.branch?.url,
+        tags: project.tags,
+        course: {
+          name: course.name,
+          url: course.url,
+        },
+      }))
+    )
+    .flat();
+};
+
+export const getProjects = async () => {
+  const courses = await getCourses();
+  const projects = parseCoursesToProjects(courses);
+
+  return projects;
+};
