0ac733b57c7507d83679dec2cdd4183429de1d6f
Nikita Kuznetsov <kuzn5298@gmail.com>
2025-01-27 16:37:35 +0100

    🏁 Finish project

--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -1,7 +1,13 @@
-import { Component, signal } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  signal,
+} from '@angular/core';
 
 import { MessagesListComponent } from './messages-list/messages-list.component';
 import { NewMessageComponent } from './new-message/new-message.component';
+import { MessagesService } from './messages.service';
 
 @Component({
   selector: 'app-messages',
@@ -9,16 +15,11 @@ import { NewMessageComponent } from './new-message/new-message.component';
   templateUrl: './messages.component.html',
   styleUrl: './messages.component.css',
   imports: [MessagesListComponent, NewMessageComponent],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MessagesComponent {
-  messages = signal<string[]>([]);
-
   get debugOutput() {
     console.log('[Messages] "debugOutput" binding re-evaluated.');
     return 'Messages Component Debug Output';
-  }
-
-  onAddMessage(message: string) {
-    this.messages.update((oldMessages) => [...oldMessages, message]);
   }
 }
