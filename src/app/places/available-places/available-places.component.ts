d92c85caa276a510cc1ebd478ca755b15fa9daeb
Nikita Kuznetsov <kuzn5298@gmail.com>
2025-01-29 19:03:40 +0100

    🛑 Add error handling

--- src/app/places/available-places/available-places.component.ts
+++ src/app/places/available-places/available-places.component.ts
@@ -1,10 +1,10 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { catchError, first, map, throwError } from 'rxjs';
 
 import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
-import { HttpClient } from '@angular/common/http';
-import { first, map } from 'rxjs';
 import { environment } from '../../../environments/environment';
 
 @Component({
@@ -16,18 +16,33 @@ import { environment } from '../../../environments/environment';
 })
 export class AvailablePlacesComponent implements OnInit {
   places = signal<Place[] | undefined>(undefined);
+  isFetching = signal(false);
+  error = signal<string | undefined>(undefined);
 
   private httpClient = inject(HttpClient);
 
   ngOnInit() {
+    this.isFetching.set(true);
     this.httpClient
       .get<{ places: Place[] }>(`${environment.apiUrl}/places`)
       .pipe(
         map((response) => response.places),
+        catchError((error) => {
+          console.log(error);
+          return throwError(() => new Error('something went wrong'));
+        }),
         first()
       )
-      .subscribe((places) => {
-        this.places.set(places);
+      .subscribe({
+        next: (places) => {
+          this.places.set(places);
+        },
+        error: (error: Error) => {
+          this.error.set(error.message);
+        },
+        complete: () => {
+          this.isFetching.set(false);
+        },
       });
   }
 }
