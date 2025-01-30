b4cd258dfab939411644fabf774350dab3d7b1b4
Nikita Kuznetsov <kuzn5298@gmail.com>
2025-01-30 02:48:40 +0100

    🚝 Add log interceptor

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
-import { provideHttpClient } from '@angular/common/http';
+import {
+  HttpHandlerFn,
+  HttpRequest,
+  HttpResponse,
+  provideHttpClient,
+  withInterceptors,
+} from '@angular/common/http';
+import { tap } from 'rxjs';
+
+const logInterceptor = (request: HttpRequest<unknown>, next: HttpHandlerFn) => {
+  console.log('Request', request.method, request.url, request.body);
+  return next(request).pipe(
+    tap({
+      next: (event) => {
+        if (event instanceof HttpResponse) {
+          console.log('Response', event.body);
+        }
+      },
+    })
+  );
+};
 
 bootstrapApplication(AppComponent, {
-  providers: [provideHttpClient()],
+  providers: [provideHttpClient(withInterceptors([logInterceptor]))],
 }).catch((err) => console.error(err));
