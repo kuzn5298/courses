d14223a689bf9a0516ce28d7b96e1458536bfdba
Nikita Kuznetsov <kuzn5298@gmail.com>
2025-01-30 15:27:38 +0100

    📋 Add form validation and saving email value

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,5 +1,13 @@
-import { Component } from '@angular/core';
+import {
+  afterNextRender,
+  afterRender,
+  Component,
+  DestroyRef,
+  inject,
+  viewChild,
+} from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
+import { debounceTime } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -9,7 +17,37 @@ import { FormsModule, NgForm } from '@angular/forms';
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
-  onSubmit(form: NgForm) {
-    console.log('Form submitted', form);
+  private formData = viewChild<NgForm>('form');
+  private destroyRef = inject(DestroyRef);
+
+  constructor() {
+    afterNextRender(() => {
+      const savedForm = localStorage.getItem('saved-login-form');
+      if (savedForm) {
+        const loadedSavedForm = JSON.parse(savedForm);
+        setTimeout(() => {
+          this.formData()?.controls['email']?.setValue(loadedSavedForm.email);
+        });
+      }
+
+      const subscription = this.formData()
+        ?.valueChanges?.pipe(debounceTime(500))
+        .subscribe(({ email }) => {
+          localStorage.setItem('saved-login-form', JSON.stringify({ email }));
+        });
+
+      this.destroyRef?.onDestroy(() => {
+        subscription?.unsubscribe();
+      });
+    });
+  }
+
+  onSubmit({ form }: NgForm) {
+    if (form.invalid) {
+      return;
+    }
+
+    const { email, password } = form.value;
+    console.log(email, password);
   }
 }
